0c89a7d789419010aad964facc5a804d234b4a73
Insoo Kim <rhwjwkdeks@gmail.com>
2021-08-17 15:04:02 -0500

    project start

---
+++ Private_Weather/Private_WeatherTests/Private_WeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  Private_WeatherTests.swift
+//  Private_WeatherTests
+//
+//  Created by Insoo Kim on 8/17/21.
+//
+
+import XCTest
+@testable import Private_Weather
+
+class Private_WeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
