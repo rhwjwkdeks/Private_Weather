28791c9765a2b9de9009842ea2d4ca99d20bfe90
Insoo Kim <rhwjwkdeks@gmail.com>
2021-08-18 21:27:46 -0500

    background

--- Private_Weather/Private_WeatherTests/Private_WeatherTests.swift
+++ Private_Weather/Private_WeatherTests/Private_WeatherTests.swift
@@ -1,33 +1,3 @@
-//
-//  Private_WeatherTests.swift
-//  Private_WeatherTests
-//
-//  Created by Insoo Kim on 8/17/21.
-//
-
-import XCTest
-@testable import Private_Weather
-
-class Private_WeatherTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2dd965f9cc898a6662cf2949a596c76f5b33597cfb3547b6fc5b489283d0d296
+size 922
