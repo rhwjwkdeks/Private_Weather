cd47fcebf66549036a71d5c2d599ed6fa519994b
Insoo Kim <rhwjwkdeks@gmail.com>
2021-08-26 19:28:39 -0500

    ui color

--- Private_Weather/Private_Weather/Model/WeatherModel.swift
+++ Private_Weather/Private_Weather/Model/WeatherModel.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5743b9e69c7837ed41ac9bfd8836637c995590426b86436f24b9f63a3c763ec0
-size 1497
+oid sha256:4aaeacd91cd2db1f88f36ef2aa564aca9df809bc3d38daac6524f0a1b0f182d4
+size 1574
